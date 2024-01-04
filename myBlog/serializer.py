f4aed30f7721afaf371801660c78578a5b590273
kkb <0furiousblade0@gmail.com>
2024-01-04 21:31:17 +0900

    [#06] Django Rest Framework - serializer 게시글 부분들 정의

--- myBlog/serializer.py
+++ myBlog/serializer.py
@@ -2,6 +2,7 @@ from .models import Post, Comment
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
+# 기본 설정
 class BlogBaseModel(serializers.ModelSerializer):
     class Meta:
         model = Post
@@ -10,9 +11,32 @@ class BlogBaseModel(serializers.ModelSerializer):
         # exclude = ['field_to_exclude']
         # 나중에 제외하고 싶은 필드 추가할 때 사용
         
+# 게시글 작성
+class PostCreateModel(BlogBaseModel):
+    class Meta(BlogBaseModel.Meta):
+        exclude = ['modified_at', 'view_counter',]
 
+# 게시글 상세보기
+class PostDetailModel(BlogBaseModel):
+    class Meta(BlogBaseModel.Meta):
 
+        fields = [
+            'title', 
+            'content',
+            'writer',
+            'created_at', 
+            'modified_at',
+            'image',
+            'view_count', 
+            ]
 
+# 게시글 업데이트
+class PostUpdateModel(BlogBaseModel):
+    class Meta(BlogBaseModel.Meta):
+        fields = ['title', 'content', 'image',]
 
-
+# 게시글 삭제
+class PostDeleteModel(BlogBaseModel):
+    class Meta(BlogBaseModel.Meta):
+        pass
 
